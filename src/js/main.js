2461008aec8dd191cdf785288e0e5cbf8308e076
Viktoriia Ostrovska <webvikysya@gmail.com>
2026-08-10 18:50:00 +0200

    create burger menu

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,21 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "../sass/style.scss";
 // init Swiper:
+
+const burger = document.querySelector(".burger"),
+  close = document.querySelector(".header__menu-close"),
+  menu = document.querySelector(".header__menu");
+
+burger.addEventListener("click", () => {
+  menu.classList.add("header__menu_active");
+  document.body.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+  menu.classList.remove("header__menu_active");
+  document.body.style.overflow = "";
+});
+
 try {
   const swiper = new Swiper(".works__slider", {
     // configure Swiper to use modules
